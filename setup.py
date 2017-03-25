7d4cdddd2db62639fbcf53fd16717ff129354c3c
Pål Grønås Drange <PGDR@statoil.com>
2017-03-25 01:49:14 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='friskby',
+    version='0.60',
+    description='The friskby moudule',
+    url='http://github.com/FriskbyBergen/friskby',
+    author='Friskby Bergen',
+    author_email='pgdr@statoil.com',
+    license='GPL v3',
+    packages=['friskby'],
+    zip_safe=False
+)
