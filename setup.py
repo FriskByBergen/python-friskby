2f6115eff1946cdc6169f191fc8840aec4062ad8
Pål Grønås Drange <PGDR@statoil.com>
2017-04-02 21:25:56 +0200

    deploy sensors submodule

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ DEPENDENCIES = ['requests', 'pyserial', 'python-dateutil']
 
 setup(
     name='friskby',
-    version='0.64.0',
+    version='0.64.1',
     description='The friskby module',
     url='http://github.com/FriskbyBergen/python-friskby',
     author='Friskby Bergen',
     keywords='friskby bergen frisk by python-friskby a-small-code air',
     author_email='pgdr@statoil.com',
     license='GNU General Public License, Version 3',
-    packages=['friskby'],
+    packages=['friskby', 'friskby/sensors'],
     zip_safe=False,
     setup_requires=DEPENDENCIES,
     install_requires=DEPENDENCIES
