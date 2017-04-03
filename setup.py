fe10f52e427b7e3dd7c5e9d3551c326748ebc8a7
Pål Grønås Drange <PGDR@statoil.com>
2017-04-04 00:16:19 +0200

    bump 0.64.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ DEPENDENCIES = ['requests', 'pyserial', 'python-dateutil']
 
 setup(
     name='friskby',
-    version='0.64.2',
+    version='0.64.3',
     description='The friskby module',
     url='http://github.com/FriskbyBergen/python-friskby',
     author='Friskby Bergen',
