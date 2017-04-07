59f58ee357a59ee333b03bc1ee1b163acff96534
Jonas G. Drange <jonas@drange.net>
2017-04-07 22:42:20 +0200

    adds pip based upgrade and systemd unit manipulation

--- friskby/friskby_manager.py
+++ friskby/friskby_manager.py
@@ -1,24 +1,107 @@
 #!/usr/bin/env python
 from __future__ import (print_function, absolute_import)
 
-import sys
-import os
 import json
-from traceback import format_exception
+import sys
 
 from .os_release import sys_info
+from time import sleep
+
+
+SYSTEMD_NAME = 'org.freedesktop.systemd1'
+SYSTEMD_OBJ = '/org/freedesktop/systemd1'
+SYSTEMD_UNIT_IFACE = 'org.freedesktop.systemd1.Unit'
+SYSTEMD_MANAGER_IFACE = 'org.freedesktop.systemd1.Manager'
+
 
 class FriskbyManager(object):
 
-    def __init__(self, device_config):
+    def __init__(self, device_config, dbus=None, pip=None,
+                 managed_packages=None, managed_services=None):
+        """Creates a Friskby manager. Given a config, the manager will upgrade
+        the client's pip packages, and appropriately reload/restart any systemd
+        units it manages.
+
+        managed_packages is a list of pip packages that it will upgrade
+            (according to user preference).
+        managed_services is a list of systemd services.
+        """
         self._config = device_config
+        self._dbus = dbus
+        self._pip = pip
+        self._managed_packages = managed_packages
+        self._managed_services = managed_services
+
+    def __run_post_install(self):
+        """
+        Checks if any known service needs a daemon-reload. If a service needs
+        it, we do a daemon-reload, wait, then restart each service.
+        """
+        if self._dbus is None:
+            print("No DBus provided, post-install can't run.")
+            sys.stdout.flush()
+            return
+
+        if len(self._managed_services) == 0:
+            print("No services to manage.")
+            sys.stdout.flush()
+            return
+
+        sysbus = self._dbus.SystemBus()
+        systemd1_obj = sysbus.get_object(SYSTEMD_NAME, SYSTEMD_OBJ)
+        manager = self._dbus.Interface(systemd1_obj, SYSTEMD_MANAGER_IFACE)
+
+        restart_units = []
+        for unit in self._managed_services:
+            unit_obj_path = manager.GetUnit(unit)
+            unit_obj = sysbus.get_object(SYSTEMD_NAME, unit_obj_path)
+            unit_proxy = self._dbus.Interface(unit_obj, SYSTEMD_UNIT_IFACE)
+            if unit_proxy.NeedDaemonReload:
+                restart_units.append(unit_proxy)
+
+        # TODO: instead of sleeping, we could track the job and wait for it
+        # to finish.
+        if len(restart_units) > 0:
+            manager.Reload()
+            sleep(5)
+
+        for u in restart_units:
+            u.Restart('replace')
 
     def install(self, config):
         # TODO should we do pip install --upgrade reuirements from config?
         config.save()
 
     def update_client(self, config):
-        return config.downloadNew() # TODO what is this now?
+        if self._pip is None:
+            print("No pip provided, post install can't run.")
+            sys.stdout.flush()
+            return
+
+        if len(self._managed_packages) == 0:
+            print("No packages to manage.")
+            sys.stdout.flush()
+            return
+
+        new_config = config.downloadNew()
+
+        update_mode = 'stable'
+        try:
+            update_mode = new_config.getChannel()
+        except KeyError:
+            pass  # No channel defined, use stable.
+
+        args = ["install", "--upgrade"]
+        if update_mode == 'latest':
+            args.append("--pre")
+
+        args.append(" ".join(self._managed_packages))
+
+        ret = self._pip.main(args=args)
+        if ret == 0:
+            self.__run_post_install()
+        else:
+            raise RuntimeError("Pip exited with non-zero code: %d" % ret)
 
     def run(self):
         # TODO this logic must be rewritten
@@ -27,7 +110,6 @@ class FriskbyManager(object):
         self._config.postVersion()
 
         self.update_client(self._config)
-
 
     @classmethod
     def get_sys_info(cls):
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,31 +1,85 @@
 from __future__ import (absolute_import, print_function)
-import os
-import json
-from unittest import TestCase
-from random import randint
 
-from friskby import FriskbyManager, DeviceConfig
+import json
+from unittest import TestCase, main as unittest_main
+
+from friskby import FriskbyManager
+
+
+class FakePip():
+
+    def __init__(self, code=None):
+        self.code = code
+        self.args = None
+
+    def main(self, args=None):
+        self.args = args
+        return self.code
+
+
+class FakeDeviceConfig():
+
+    def __init__(self, channel=None):
+        self.channel = channel
+
+    def downloadNew(self):
+        return self
+
+    def getChannel(self):
+        if self.channel is None:
+            raise KeyError("no channel")
+        return self.channel
 
 
 class FriskbyManagerTest(TestCase):
 
-    def setUp(self):
-        _tmp_f = '/tmp/%d' % randint(2**30, 2**32)
-        url_ = "https://friskby.herokuapp.com/sensor/api/device/FriskPITest/"
-        deviceconfig = DeviceConfig.download(url_, post_key="xxx")
-        deviceconfig.save(filename=_tmp_f)
-        self.cfg = deviceconfig
-
-
-    def test_load(self):
-        _ = FriskbyManager(self.cfg)
-
     def test_sysinfo(self):
         sys_info = FriskbyManager.get_sys_info()
-        self.assertTrue('sysname'  in sys_info)
+        self.assertTrue('sysname' in sys_info)
         self.assertTrue('requests' in sys_info)
-        self.assertTrue('python'   in sys_info)
+        self.assertTrue('python' in sys_info)
 
         sys_info = FriskbyManager.get_sys_info()
         sys_info = json.dumps(sys_info, indent=4, sort_keys=True)
         print(sys_info)
+
+    def test_nominal_and_stable_update(self):
+        cfg = FakeDeviceConfig(channel="stable")
+        pip = FakePip(code=0)
+
+        manager = FriskbyManager(cfg, pip=pip, managed_packages=["package1"])
+        manager.update_client(cfg)
+
+        self.assertEqual(pip.args, ["install", "--upgrade", "package1"])
+
+    def test_failed_stable_update(self):
+        cfg = FakeDeviceConfig(channel="stable")
+        pip = FakePip(code=255)
+
+        manager = FriskbyManager(cfg, pip=pip, managed_packages=["package1"])
+
+        with self.assertRaises(RuntimeError):
+            manager.update_client(cfg)
+
+    def test_nominal_lastes_update(self):
+        cfg = FakeDeviceConfig(channel="latest")
+        pip = FakePip(code=0)
+
+        manager = FriskbyManager(cfg, pip=pip, managed_packages=["package1"])
+        manager.update_client(cfg)
+
+        self.assertEqual(pip.args, ["install", "--upgrade", "--pre",
+                                    "package1"])
+
+    def test_update_when_no_channel_provided(self):
+        cfg = FakeDeviceConfig()
+        pip = FakePip(code=0)
+
+        manager = FriskbyManager(cfg, pip=pip, managed_packages=["package1"])
+        manager.update_client(cfg)
+
+        self.assertEqual(pip.args, ["install", "--upgrade", "package1"])
+
+
+if __name__ == '__main__':
+    unittest_main()
