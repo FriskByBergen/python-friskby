7d9b3cf37c99fd7f922dbe01f049d49227062404
Pål Grønås Drange <PGDR@statoil.com>
2017-04-09 23:25:34 +0200

    travis deploy to pip

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ DEPENDENCIES = ['requests', 'pyserial', 'python-dateutil', 'pip']
 
 setup(
     name='friskby',
-    version='0.66.0',
+    version='0.70b1',
     description='The friskby module',
     url='http://github.com/FriskbyBergen/python-friskby',
     author='Friskby Bergen',
